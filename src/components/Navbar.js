8ea81f9736f6cdb723d19effce442f16fafa4792
Satish627 <Satish.grg627@gmail.com>
2022-02-28 14:47:44 +0100

    first commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Navbar(){
+    return(
+        <nav >
+            <img src= "images/airbnb-logo.png" alt="logo of the app " className="nav--logo"/>
+        </nav>
+    )
+}
